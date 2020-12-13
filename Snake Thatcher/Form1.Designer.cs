b2589d8ad28be0116859f6d37f0b532e877abdbd
Max Power <palladidrago@gmail.com>
2020-12-13 22:26:27 +0200

    Add Tohu VahaVohu

--- Snake Thatcher/Form1.Designer.cs
+++ Snake Thatcher/Form1.Designer.cs
@@ -32,20 +32,21 @@
             this.snake = new System.Windows.Forms.Label();
             this.apple1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.apple8 = new System.Windows.Forms.Label();
             this.apple2 = new System.Windows.Forms.Label();
-            this.apple10 = new System.Windows.Forms.Label();
-            this.apple7 = new System.Windows.Forms.Label();
             this.apple4 = new System.Windows.Forms.Label();
             this.apple6 = new System.Windows.Forms.Label();
             this.apple5 = new System.Windows.Forms.Label();
-            this.apple9 = new System.Windows.Forms.Label();
             this.apple3 = new System.Windows.Forms.Label();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.timer3 = new System.Windows.Forms.Timer(this.components);
             this.timer4 = new System.Windows.Forms.Timer(this.components);
             this.timer5 = new System.Windows.Forms.Timer(this.components);
             this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.label_MultiColor = new System.Windows.Forms.Label();
+            this.pillar1 = new System.Windows.Forms.Label();
+            this.pillar2 = new System.Windows.Forms.Label();
+            this.base2 = new System.Windows.Forms.Label();
+            this.base1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // snake
@@ -53,15 +54,15 @@
             this.snake.BackColor = System.Drawing.Color.Lime;
             this.snake.Location = new System.Drawing.Point(12, 20);
             this.snake.Name = "snake";
-            this.snake.Size = new System.Drawing.Size(69, 33);
+            this.snake.Size = new System.Drawing.Size(37, 33);
             this.snake.TabIndex = 0;
             // 
             // apple1
             // 
             this.apple1.BackColor = System.Drawing.Color.Red;
-            this.apple1.Location = new System.Drawing.Point(190, 31);
+            this.apple1.Location = new System.Drawing.Point(178, 20);
             this.apple1.Name = "apple1";
-            this.apple1.Size = new System.Drawing.Size(22, 22);
+            this.apple1.Size = new System.Drawing.Size(37, 33);
             this.apple1.TabIndex = 0;
             // 
             // timer1
@@ -69,119 +70,138 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // apple8
-            // 
-            this.apple8.BackColor = System.Drawing.Color.Red;
-            this.apple8.Location = new System.Drawing.Point(25, 165);
-            this.apple8.Name = "apple8";
-            this.apple8.Size = new System.Drawing.Size(22, 22);
-            this.apple8.TabIndex = 0;
-            // 
             // apple2
             // 
             this.apple2.BackColor = System.Drawing.Color.Red;
-            this.apple2.Location = new System.Drawing.Point(766, 31);
+            this.apple2.Location = new System.Drawing.Point(766, 20);
             this.apple2.Name = "apple2";
-            this.apple2.Size = new System.Drawing.Size(22, 22);
+            this.apple2.Size = new System.Drawing.Size(37, 33);
             this.apple2.TabIndex = 0;
-            // 
-            // apple10
-            // 
-            this.apple10.BackColor = System.Drawing.Color.Red;
-            this.apple10.Location = new System.Drawing.Point(12, 419);
-            this.apple10.Name = "apple10";
-            this.apple10.Size = new System.Drawing.Size(22, 22);
-            this.apple10.TabIndex = 0;
-            // 
-            // apple7
-            // 
-            this.apple7.BackColor = System.Drawing.Color.Red;
-            this.apple7.Location = new System.Drawing.Point(230, 317);
-            this.apple7.Name = "apple7";
-            this.apple7.Size = new System.Drawing.Size(22, 22);
-            this.apple7.TabIndex = 0;
             // 
             // apple4
             // 
             this.apple4.BackColor = System.Drawing.Color.Red;
-            this.apple4.Location = new System.Drawing.Point(715, 297);
+            this.apple4.Location = new System.Drawing.Point(19, 419);
             this.apple4.Name = "apple4";
-            this.apple4.Size = new System.Drawing.Size(22, 22);
+            this.apple4.Size = new System.Drawing.Size(30, 30);
             this.apple4.TabIndex = 0;
             // 
             // apple6
             // 
             this.apple6.BackColor = System.Drawing.Color.Red;
-            this.apple6.Location = new System.Drawing.Point(544, 165);
+            this.apple6.Location = new System.Drawing.Point(492, 87);
             this.apple6.Name = "apple6";
-            this.apple6.Size = new System.Drawing.Size(22, 22);
+            this.apple6.Size = new System.Drawing.Size(30, 30);
             this.apple6.TabIndex = 0;
             // 
             // apple5
             // 
             this.apple5.BackColor = System.Drawing.Color.Red;
-            this.apple5.Location = new System.Drawing.Point(463, 267);
+            this.apple5.Location = new System.Drawing.Point(12, 87);
             this.apple5.Name = "apple5";
-            this.apple5.Size = new System.Drawing.Size(22, 22);
+            this.apple5.Size = new System.Drawing.Size(30, 30);
             this.apple5.TabIndex = 0;
-            // 
-            // apple9
-            // 
-            this.apple9.BackColor = System.Drawing.Color.Red;
-            this.apple9.Location = new System.Drawing.Point(115, 317);
-            this.apple9.Name = "apple9";
-            this.apple9.Size = new System.Drawing.Size(22, 22);
-            this.apple9.TabIndex = 0;
             // 
             // apple3
             // 
             this.apple3.BackColor = System.Drawing.Color.Red;
-            this.apple3.Location = new System.Drawing.Point(766, 419);
+            this.apple3.Location = new System.Drawing.Point(773, 419);
             this.apple3.Name = "apple3";
-            this.apple3.Size = new System.Drawing.Size(22, 22);
+            this.apple3.Size = new System.Drawing.Size(30, 30);
             this.apple3.TabIndex = 0;
             // 
             // timer2
             // 
-            this.timer2.Enabled = true;
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // timer3
             // 
-            this.timer3.Enabled = true;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
             // 
             // timer4
             // 
-            this.timer4.Enabled = true;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
             // 
             // timer5
             // 
-            this.timer5.Enabled = true;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
             // 
             // timer6
             // 
-            this.timer6.Enabled = true;
+            this.timer6.Interval = 10;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // label_MultiColor
+            // 
+            this.label_MultiColor.AutoSize = true;
+            this.label_MultiColor.Font = new System.Drawing.Font("Mistral", 72F);
+            this.label_MultiColor.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label_MultiColor.Location = new System.Drawing.Point(73, 178);
+            this.label_MultiColor.Name = "label_MultiColor";
+            this.label_MultiColor.Size = new System.Drawing.Size(419, 143);
+            this.label_MultiColor.TabIndex = 0;
+            this.label_MultiColor.Text = "YOU WIN";
+            this.label_MultiColor.Visible = false;
+            // 
+            // pillar1
+            // 
+            this.pillar1.BackColor = System.Drawing.Color.Red;
+            this.pillar1.Location = new System.Drawing.Point(637, 419);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(100, 23);
+            this.pillar1.TabIndex = 1;
+            this.pillar1.Visible = false;
+            // 
+            // pillar2
+            // 
+            this.pillar2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pillar2.Location = new System.Drawing.Point(48, 63);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(64, 37);
+            this.pillar2.TabIndex = 2;
+            this.pillar2.Visible = false;
+            // 
+            // base2
+            // 
+            this.base2.BackColor = System.Drawing.Color.Yellow;
+            this.base2.Location = new System.Drawing.Point(48, 40);
+            this.base2.Name = "base2";
+            this.base2.Size = new System.Drawing.Size(100, 23);
+            this.base2.TabIndex = 3;
+            this.base2.Visible = false;
+            // 
+            // base1
+            // 
+            this.base1.BackColor = System.Drawing.Color.Purple;
+            this.base1.Location = new System.Drawing.Point(673, 442);
+            this.base1.Name = "base1";
+            this.base1.Size = new System.Drawing.Size(64, 37);
+            this.base1.TabIndex = 4;
+            this.base1.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowText;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(842, 493);
+            this.Controls.Add(this.base1);
+            this.Controls.Add(this.base2);
+            this.Controls.Add(this.pillar2);
+            this.Controls.Add(this.pillar1);
+            this.Controls.Add(this.label_MultiColor);
             this.Controls.Add(this.snake);
             this.Controls.Add(this.apple6);
             this.Controls.Add(this.apple4);
             this.Controls.Add(this.apple3);
-            this.Controls.Add(this.apple9);
             this.Controls.Add(this.apple5);
-            this.Controls.Add(this.apple7);
-            this.Controls.Add(this.apple10);
             this.Controls.Add(this.apple2);
-            this.Controls.Add(this.apple8);
             this.Controls.Add(this.apple1);
             this.Name = "Form1";
             this.Text = "Snake Thatcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -190,20 +210,21 @@
         private System.Windows.Forms.Label snake;
         private System.Windows.Forms.Label apple1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label apple8;
         private System.Windows.Forms.Label apple2;
-        private System.Windows.Forms.Label apple10;
-        private System.Windows.Forms.Label apple7;
         private System.Windows.Forms.Label apple4;
         private System.Windows.Forms.Label apple6;
         private System.Windows.Forms.Label apple5;
-        private System.Windows.Forms.Label apple9;
         private System.Windows.Forms.Label apple3;
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.Timer timer3;
         private System.Windows.Forms.Timer timer4;
         private System.Windows.Forms.Timer timer5;
         private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.Label label_MultiColor;
+        private System.Windows.Forms.Label pillar1;
+        private System.Windows.Forms.Label pillar2;
+        private System.Windows.Forms.Label base2;
+        private System.Windows.Forms.Label base1;
     }
 }
 
--- Snake Thatcher/Form1.cs
+++ Snake Thatcher/Form1.cs
@@ -15,28 +15,45 @@ namespace Snake_Thatcher
         public Form1()
         {
             InitializeComponent();
-            for (int i = 2; i <= 10; i++)
+            for (int i = 2; i <= 6; i++)
             {
-                this.Controls[$"apple{i}"].Visible=false;
-                
+                this.Controls[$"apple{i}"].Visible = false;
+
             }
         }
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer("humor.wav");
+        const int speed = 10;
+        const int gain = 20;
+
+        const int height = 33;
+        int width = 37;
         int curApple = 1;
+        int funCount = 0;
         private void timer1_Tick(object sender, EventArgs e)
         {
-            snake.Left+=10;
+            snake.Left += speed;
             if (snake.Right > this.Controls["apple" + curApple].Right)
             {
+                
                 this.Controls["apple" + curApple].Visible = false;
                 curApple++;
                 this.Controls["apple" + curApple].Visible = true;
+
+                if (curApple == 2)
+                {
+                    snake.Width += gain;
+                    width += gain;
+                    snake.Left -= gain;
+                }
+                int temp;
                 if (curApple == 3)
                 {
-                    snake.Height *= 2;
-                    snake.Width /= 2;
-                    snake.Left += snake.Width;
+                    temp = snake.Height;
+                    snake.Height = snake.Width + gain;
+                    snake.Left += snake.Width - temp;
+                    snake.Width = temp;
                     timer1.Enabled = false;
-                    timer2.Enabled = true; 
+                    timer2.Enabled = true;
                 }
             }
         }
@@ -44,12 +61,154 @@ namespace Snake_Thatcher
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-           
+            snake.Top += speed;
+
+            if (snake.Top + snake.Height > this.Controls["apple" + curApple].Top + this.Controls["apple" + curApple].Height)
+            {
+                this.Controls["apple" + curApple].Visible = false;
+                curApple++;
+                this.Controls["apple" + curApple].Visible = true;
+                if (curApple == 4)
+                {
+                    int temp = snake.Width;
+                    snake.Top += height ;
+                    snake.Left = snake.Left + snake.Width / 2 - snake.Height;
+
+                    snake.Width = snake.Height + gain;
+                    width += gain;
+                    snake.Height = temp;
+
+                    timer2.Enabled = false;
+                    timer3.Enabled = true;
+                }
+            }
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            int temp;
+            snake.Left -= speed;
+            if (snake.Left < this.Controls["apple" + curApple].Left)
+            {
+                this.Controls["apple" + curApple].Visible = false;
+                curApple++;
+                this.Controls["apple" + curApple].Visible = true;
+                temp = snake.Height;
+                snake.Height = snake.Width+gain;
+                width += gain;
+                
+                snake.Width = temp;
+                snake.Top += -snake.Height+ snake.Width-3;
+                
+                timer3.Enabled = false;
+                timer4.Enabled = true;
+                
+            }
+
+
+
 
         }
-        private void ChangeApple()
+
+        private void timer4_Tick(object sender, EventArgs e)
         {
-            MessageBox.Show(curApple.ToString());
+            int temp;
+            snake.Top -= speed;
+            if (snake.Top < this.Controls["apple" + curApple].Top)
+            {
+                this.Controls["apple" + curApple].Visible = false;
+                curApple++;
+                this.Controls["apple" + curApple].Visible = true;
+                temp = snake.Width;
+                snake.Width = snake.Height;
+                snake.Height = temp;
+                
+
+                timer4.Enabled = false;
+                timer5.Enabled = true;
+
+            }
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            snake.Left += speed;
+            if (snake.Right > this.Controls["apple" + curApple].Right)
+            {
+                this.Controls["apple" + curApple].Visible = false;
+                curApple++;
+                timer5.Enabled = false;
+                snake.Left = 0;
+                snake.Top = 0;
+                player.Play();
+                timer6.Enabled = true;
+            }
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+           
+            if (snake.Height < 5000) {
+                
+                snake.Height += 10;
+                snake.Width += 10;
+                
+            }
+            label_MultiColor.Visible = true;
+            Random rnd = new Random();
+
+            pillar1.Visible = true;
+            pillar2.Visible = true;
+            base1.Visible = true;
+            base2.Visible = true;
+            pillar1.Height += 2;
+            pillar1.Top -= 2;
+            pillar2.Height +=2;
+            base1.Width += 2;
+            base1.Left -= 2;
+            base2.Width += 2;
+            switch (rnd.Next(1, 5))
+            {
+                case 1:
+
+                    label_MultiColor.ForeColor = Color.Red;
+                    
+                    break;
+                case 2:
+
+                    label_MultiColor.BackColor = Color.Green;
+                    
+                    break;
+                case 3:
+
+                    label_MultiColor.ForeColor = Color.Blue;
+                    
+                    break;
+                case 4:
+
+                    label_MultiColor.BackColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+            }
+            
+            switch (funCount)
+            {
+                case 100:
+                    label_MultiColor.Font = new Font("Mistral", 68);break;
+                case 200:
+                    label_MultiColor.Font = new Font("Monotxt", 50);break;
+                case 300:
+                    label_MultiColor.Font = new Font("Mistral", 80);break;
+                case 400:
+                    label_MultiColor.Font = new Font("Papyrus", 75);funCount = 0 ;break;
+        }
+            funCount++;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
     }
